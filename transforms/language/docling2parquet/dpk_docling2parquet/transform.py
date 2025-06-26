833900a2ad4d55647ea15b8d69d2caa117046df5
SHAHROKH DAIJAVAD <shahrokh@us.ibm.com>
2025-06-26 14:15:03 -0700

    created expected results and added case insensitivity
    Signed-off-by: SHAHROKH DAIJAVAD <shahrokh@us.ibm.com>

--- transforms/language/docling2parquet/dpk_docling2parquet/transform.py
+++ transforms/language/docling2parquet/dpk_docling2parquet/transform.py
@@ -266,7 +266,7 @@ class Docling2ParquetTransform(AbstractBinaryTransform):
 
     def _detect_mime(self, file_name: str, content_bytes: bytes) -> tuple[str|None, str]:
         kind = filetype.guess(content_bytes)
-        ext = TransformUtils.get_file_extension(file_name)[1]
+        ext = TransformUtils.get_file_extension(file_name)[1].lower()
         if kind is not None:
             mime = kind.mime
             ext = kind.extension
